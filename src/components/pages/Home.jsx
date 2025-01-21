fe7f3dda60829424a739474aa42d68d9df37f748
Rifat Jayed <rifat.jayed2000@gmail.com>
2025-01-21 18:18:11 +0600

    server side deploy done and api change done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,7 +15,9 @@ const Home = () => {
   //   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/jobs")
+    // https://job-server-backend.onrender.com
+    // http://localhost:5000/api/jobs
+    fetch("https://job-server-backend.onrender.com/api/jobs")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -44,47 +46,42 @@ const Home = () => {
           </p>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-            {jobs.slice(0, 6).map(
-              (
-                job,
-                index // শুধুমাত্র প্রথম ৬টি দেখাবে
-              ) => (
-                <div
-                  key={index}
-                  className="p-4 border rounded-lg shadow-md hover:shadow-lg transition"
-                >
-                  <h2 className="text-lg font-semibold text-gray-800">
-                    {job.title}
-                  </h2>
+            {jobs.slice(0, 6).map((job, index) => (
+              <div
+                key={index}
+                className="p-4 border rounded-lg shadow-md hover:shadow-lg transition"
+              >
+                <h2 className="text-lg font-semibold text-gray-800">
+                  {job.title}
+                </h2>
 
-                  <h3 className="text-left">{job.job_type}</h3>
-                  <div className="flex items-center">
-                    <img
-                      src={job.company_logo}
-                      alt={job.company}
-                      className="w-12 h-12 object-cover rounded-full border"
-                    />
-                    <div>
-                      <p className="text-sm text-gray-500 ml-2">
-                        {job.company_name}
-                      </p>
-                      <p>{job.candidate_required_location}</p>
-                    </div>
-                  </div>
-
-                  <p className="text-gray-600">{job.location}</p>
-                  <p className="text-gray-500 mt-2">{job.company}</p>
-                  <div className="flex  justify-around">
-                    <button className="mt-4  text-[#303030] px-4 py-2 rounded border border-[#6300B3]">
-                      View Details
-                    </button>
-                    <button className="mt-4  text-white px-4 py-2 rounded bg-[#6300B3]">
-                      Apply Now{" "}
-                    </button>
+                <h3 className="text-left">{job.job_type}</h3>
+                <div className="flex items-center">
+                  <img
+                    src={job.company_logo}
+                    alt={job.company}
+                    className="w-12 h-12 object-cover rounded-full border"
+                  />
+                  <div>
+                    <p className="text-sm text-gray-500 ml-2">
+                      {job.company_name}
+                    </p>
+                    <p>{job.candidate_required_location}</p>
                   </div>
                 </div>
-              )
-            )}
+
+                <p className="text-gray-600">{job.location}</p>
+                <p className="text-gray-500 mt-2">{job.company}</p>
+                <div className="flex  justify-around">
+                  <button className="mt-4  text-[#303030] px-4 py-2 rounded border border-[#6300B3]">
+                    View Details
+                  </button>
+                  <button className="mt-4  text-white px-4 py-2 rounded bg-[#6300B3]">
+                    Apply Now{" "}
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
         <div className="text-center">
